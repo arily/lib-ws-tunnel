9ccb6cdf897cbe2877d68d9d0177d91afa915d72
arily <lbsaaaaa@gmail.com>
2020-01-26 23:29:11 +0900

    sb

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.WsConnectionContainer = require('./requires/wsConnectionContainer');
-module.WsServer = require('./requires/wsServer');
-module.WsTunnel = require('./requires/wsTunnel');
-module.WsTunnelProxifier = require('./requires/wsTunnelProxifier');
+exports.WsConnectionContainer = require('./requires/wsConnectionContainer');
+exports.WsServer = require('./requires/wsServer');
+exports.WsTunnel = require('./requires/wsTunnel');
+exports.WsTunnelProxifier = require('./requires/wsTunnelProxifier');
